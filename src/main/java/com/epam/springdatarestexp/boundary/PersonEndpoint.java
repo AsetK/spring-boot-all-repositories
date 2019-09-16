d44f26dc454b00cfa4aaa82294bbf3b6e6b2788f
Asset_Kenezhanov <bysskc@mail.ru>
2019-09-16 14:47:50 +0600

    5-commit: Spring Data Rest + Pageable(and JSON) + how @RequiredArgsConstructor works on interface + How Spring Data proxy-repository implementation looks like

--- src/main/java/com/epam/springdatarestexp/boundary/PersonEndpoint.java
+++ src/main/java/com/epam/springdatarestexp/boundary/PersonEndpoint.java
@@ -8,11 +8,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("rest")
 public class PersonEndpoint {
 
     @Autowired
--- src/main/java/com/epam/springdatarestexp/control/PersonRepositoryService.java
+++ src/main/java/com/epam/springdatarestexp/control/PersonRepositoryService.java
@@ -6,22 +6,52 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
 public class PersonRepositoryService {
 
-    private final SomeInterface someInterface;      // Cheching how @RequiredArgsConstructor works on interface. There should be bean of class that implements this interface
+    private final SomeInterface someInterface;      // Cheching how @RequiredArgsConstructor works on interface. There should be bean of class that implements this interface.
+                                                    // This bean will be injected throw constructor(Spring can inject beans throw constructor parameters without @Autowired annotation)
     private final PersonRepository personRepository;
 
     public Page<Person> findAll(Pageable pageable){
         someInterface.check();                      // Cheching how @RequiredArgsConstructor works on interface. There should be bean of class that implements this interface
+        System.out.println(personRepository.getClass());
         return personRepository.findAll(pageable);
     }
 
     public List<Person> findAll(){
+        this.proxyInfo();
         return personRepository.findAll();
+    }
+
+    private void proxyInfo(){
+        System.out.println("Class name: " + personRepository.getClass());
+        System.out.println("Fields: " + personRepository.getClass().getFields().length); //Нет полей - нет обьекта которго можно пнуть(прокси паттерн). Значит не проекси паттерн?
+        Arrays.stream(personRepository.getClass().getFields()).forEach(f-> System.out.println("- " + f.getName()));
+        System.out.println("Methods: ");            Arrays.stream(personRepository.getClass().getMethods()).forEach(m-> System.out.println("- " + m.getName()));
+        System.out.println("Declared Methods: ");   Arrays.stream(personRepository.getClass().getDeclaredMethods()).forEach(dm-> System.out.println("- " + dm.getName()));
+
+        System.out.println(personRepository.getClass().getMethods().length);
+        System.out.println(personRepository.getClass().getDeclaredMethods().length);
+
+        System.out.println("Filter methods(excluding inherited methods, but including interface methods:");
+        Method[] methods = personRepository.getClass().getMethods();
+        Method[] declaredMethods = personRepository.getClass().getDeclaredMethods();
+
+        Set<Method> methodsSet=  new HashSet<Method>(Arrays.asList(methods));
+        Set<Method> declaredMethodsSet=  new HashSet<Method>(Arrays.asList(declaredMethods));
+
+        methodsSet.removeAll(declaredMethodsSet);
+        methodsSet.stream().forEach(method -> System.out.println("- " + method.getName()));
+        Object o = new Object();
+
     }
 
 
